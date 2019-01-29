e2a1d609c2dccf7e22e49c9c88bd82404fda0371
Kwahn20 <kevin.w.ahn@gmail.com>
2019-01-28 21:19:49 -0500

    updated contact info

--- contactValidation.js
+++ contactValidation.js
@@ -4,26 +4,34 @@ let emailRegex = /^([\w\d\-\.]+)@{1}(([\w\d\-]{1,67})|([\w\d\-]+\.[\w\d\-]{1,67}
 $(document).ready(function(){
 	// same as above real time validation
 	$('.validEmail').addClass('hidden');
-	$('#emailValid').keyup(function(){
+	$('#email').keyup(function(){
 		if ($(this).val().match(emailRegex)){
 			$('.invalid').remove(); // removes old error messages
 			$('.validEmail').removeClass('hidden');
 			$('.validEmail').show();
 		}
+		else {
+            $('.validEmail').addClass('hidden');
+        }
 	});
     $(".contactValidator").submit(function(e){
 		$('.invalid').remove(); // removes old error messages
-		e.preventDefault();
-        	let email = $('#emailValid').val();
-        	if (email.length < 1 ||!email.match(emailRegex)){
-          	$('#emailValid').after("<span class='invalid'> Please enter a valid email</span>");
-          	$('.validEmail').addClass('hidden'); // hides the valid text line, no need for regex matching here
-        	}
+        let email = $('#email').val();
+        let name = $('#name').val();
+        let comment = $('#msgContent').val();
+        if (email.length < 1 ||!email.match(emailRegex)|| name.length < 1 || comment.length < 1){
+                e.preventDefault();
+            	$('#email').after("<span class='invalid'>Please Fill out all Fields</span>");
+             	$('.validEmail').addClass('hidden'); // hides the valid text line, no need for regex matching here
+
+        }
 		else {
 			$.ajax({
 				type:'post',
-				url:'emailGenerator.php',
-				success:function(response){ 
+				url:'http://emailGenerator.php',
+				success:function(){
+					$('.invalid').innerHTML("Message has been sent");
+					$('.invalid').show();
 				}
 			});
 		}
--- emailGenerator.php
+++ emailGenerator.php
@@ -1,18 +1,17 @@
-<?php 
-$contents = file_get_contents("contact.html");
+<?php
+if ($_POST) {
+    /*Grabs the information from the form from the HTML*/
+    $nameInput = $_POST['userName'];
+    $emailInput = $_POST['email'];
+    $commentInput = $_POST['commentContent'];
 
-/*Grabs the information from the form from the HTML*/
-$nameInput = $_POST['userName'];
-$emailInput = $_POST['email'];
-$commentInput = $_POST['commentContent'];
+    $finalMsg = "Name: " . $nameInput;
+    $finalMsg .= "\nEmail: " . $emailInput;
+    $finalMsg .= "\nMessage Content: " . $commentInput;
+    $finalMsg = wordwrap($finalMsg, 70, "\r\n");
+    mail('kwahn16yootube@gmail.com',"Marquis Properties Contact:",$finalMsg);
+    header("Location: http://cs325.colby.edu/kwahn20/Marquis_Properties/contact.html");
 
-$finalMsg = "Name: ".$nameInput;
-$finalMsg .= "\nEmail: ".$emailInput;
-$finalMsg .= "\nMessage Content: ".$commentInput;
-$finalMsg = wordwrap($finalMsg, 70, "\r\n");
-
-//mail('kwahn16yootube@gmail.com',"Marquis Properties Contact:",$finalMsg);
-echo file_get_contents("contact.html");
-
+}
 ?>
 
