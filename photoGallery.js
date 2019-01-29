7bc07122420fcbac0646ce4175e2b79e04e04b3e
Kwslag20 <kwslag20@colby.edu>
2019-01-29 09:51:39 -0500

    Property Pages added

--- photoGallery.js
+++ photoGallery.js
@@ -16,11 +16,31 @@ $(document).ready(function(){
             $("#numOfnum").innerHTML = numOfnum;
         }
     });
-    $("button").click(function(){
+    $("#rightButton").click(function(){
         $.ajax({
             type: 'POST',
             url: 'http://cs325.colby.edu/kwahn/CS325MP/photoGallery.php',
-            data: {idx: $("#numOfnum").innerHTML },
+            data: {idx: $("#numOfnum").innerHTML, dir: 1 },
+            success: function(data) {
+                let i = 0;
+                let idx = 0;
+                for(i = 0; i < data.length; i++){
+                    if(data[i] == '$'){
+                        idx = i;
+                    }
+                }
+                let address = data.slice(0, idx);
+                let numOfnum = data.slice(idx);
+                $("#galleryPic").attr("src", address);
+                $("#numOfnum").innerHTML = numOfnum;
+            }
+        });
+    });
+    $("#leftButton").click(function(){
+        $.ajax({
+            type: 'POST',
+            url: 'http://cs325.colby.edu/kwahn/CS325MP/photoGallery.php',
+            data: {idx: $("#numOfnum").innerHTML, dir: -1 },
             success: function(data) {
                 let i = 0;
                 let idx = 0;
--- photoGallery.php
+++ photoGallery.php
@@ -7,9 +7,18 @@
  */
 
     $numOfnums = $_POST["idx"];
-    $idx = (int)substr($numOfnums,0,2) + 1;
+    $dir = $_POST["dir"];
     $images = scandir("/web/docs/kwahn20/CS325MP/ASSETS/Regatta_pics/", 1);
-    echo "http://cs325.colby.edu/kwahn/CS325MP/CS325MP/ASSETS/Regatta_pics/$images[$idx]" + " $" + "$idx" + substr($numOfnums, 2);
+    $idx = (int)substr($numOfnums,0,2) + $dir;
+    if($idx < 1){
+        $idx = 1;
+    }
+    if($idx > (count($images) - 3)){
+        $idx = (count($images) - 3);
+    }
+    $numOfnums = ($idx + 1) + "/" + (count($images) - 2);
+    $images = scandir("/web/docs/kwahn20/CS325MP/ASSETS/Regatta_pics/", 1);
+    echo "http://cs325.colby.edu/kwahn/CS325MP/CS325MP/ASSETS/Regatta_pics/$images[$idx]" + " $" + $numOfnums;
 
 
 
